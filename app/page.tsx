08c5ce80fd6dc7ecd611e37ac5dfe225d787889f
jinoc <choiae369@gmail.com>
2024-03-04 15:59:06 +0900

    Feat: 회원가입 폼 스키마 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import SignupForm from '@/components/signupForm/SignupForm';
 import SwitchThemeButton from '@/components/switchTheme/SwitchThemeButton';
 
 export default function Home() {
   return (
     <main>
       <SwitchThemeButton />
+      <SignupForm />
     </main>
   );
 }
