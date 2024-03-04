f41e219cbd813e0cebaa55a68ba9be5f0b1f252a
jinoc <choiae369@gmail.com>
2024-03-04 16:43:32 +0900

    Fix: signupFormSchema 수정

--- components/signupForm/SignupForm.tsx
+++ components/signupForm/SignupForm.tsx
@@ -1,10 +1,60 @@
+'use client';
+
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { signupFormSchema } from '@/schema/signupFormSchema';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { Input } from '../ui/input';
+
+import type { z } from 'zod';
 
 const SignupForm = () => {
+  const signupForm = useForm<z.infer<typeof signupFormSchema>>({
+    resolver: zodResolver(signupFormSchema),
+    mode: 'onChange',
+    defaultValues: {
+      username: '',
+      email: '',
+      phone: '',
+      role: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+  const {
+    formState: { errors },
+  } = signupForm;
+  console.log(errors);
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Form {...signupForm}>
+      <form className="space-y-8">
+        <FormField
+          control={signupForm.control}
+          name="username"
+          render={({ field, formState }) => (
+            <FormItem>
+              <FormLabel>이름</FormLabel>
+              <FormControl>
+                <Input placeholder="홍길동" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        ></FormField>
+      </form>
+    </Form>
   );
 };
 
