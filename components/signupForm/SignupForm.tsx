7c8e9b68ca23097e862d781ea2976f8e305b56f1
jinoc <choiae369@gmail.com>
2024-03-04 17:05:10 +0900

    Feat: 회원가입 폼 input들 추가

--- components/signupForm/SignupForm.tsx
+++ components/signupForm/SignupForm.tsx
@@ -32,29 +32,108 @@ const SignupForm = () => {
       confirmPassword: '',
     },
   });
-  const {
-    formState: { errors },
-  } = signupForm;
-  console.log(errors);
 
   return (
-    <Form {...signupForm}>
-      <form className="space-y-8">
-        <FormField
-          control={signupForm.control}
-          name="username"
-          render={({ field, formState }) => (
-            <FormItem>
-              <FormLabel>이름</FormLabel>
-              <FormControl>
-                <Input placeholder="홍길동" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        ></FormField>
-      </form>
-    </Form>
+    <section className=" absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 space-y-8">
+      <Form {...signupForm}>
+        <form className="w-[380px] rounded-lg border bg-card text-card-foreground shadow-sm p-6">
+          <div className="flex flex-col space-y-1.5 pb-6">
+            <h3 className="text-2xl font-semibold leading-none tracking-tight">
+              계정을 생성합니다
+            </h3>
+            <p className="text-sm text-muted-foreground">
+              필수 정보를 입력해 주세요.
+            </p>
+          </div>
+          <FormField
+            control={signupForm.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>이름</FormLabel>
+                <FormControl>
+                  <Input placeholder="홍길동" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signupForm.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>이메일</FormLabel>
+                <FormControl>
+                  <Input placeholder="example@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signupForm.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>연락처</FormLabel>
+                <FormControl>
+                  <Input placeholder="01012345678" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signupForm.control}
+            name="role"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>역할</FormLabel>
+                <FormControl>
+                  <Input placeholder="홍길동" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signupForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>비밀번호</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="비밀번호를 입력해 주세요."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signupForm.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>비밀번호 확인</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="비밀번호를 다시 입력해 주세요."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </section>
   );
 };
 
