d70b6c017d10598f805c2e41092898f10bb7ba3d
jinoc <choiae369@gmail.com>
2024-03-07 13:25:24 +0900

    Refactor: SignupFormInput을 FormNormalInput으로 변경
    - 로그인 페이지에서도 사용하기 위하여 이름 및 Props 변경

--- schema/signinFormSchema.ts
+++ schema/signinFormSchema.ts
@@ -3,6 +3,6 @@ import { z } from 'zod';
 import { passwordRegExp } from './signupFormSchema';
 
 export const signinFormSchema = z.object({
-  email: z.string().email({ message: '올바른 이메일이 아닙니다.' }),
+  email: z.string().email(),
   password: z.string().min(6).max(50).regex(passwordRegExp),
 });
