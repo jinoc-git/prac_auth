966fe35c200cdfec83461f090f8ca14b8713096a
jinoc <choiae369@gmail.com>
2024-03-07 15:25:52 +0900

    Feat: root path에서 로그인 여부를 확인하여 signin으로 redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
 import Introduce from '@/components/home-content/introduce/Introduce';
 
-export default function Home() {
+import type { Database } from '@/lib/database.types';
+
+export default async function Home() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) redirect('/signin');
+
   return (
     <main className="flex-center min-h-[calc(100vh-88px)]">
-      <Introduce></Introduce>
+      <Introduce />
     </main>
   );
 }
