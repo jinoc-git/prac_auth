ffdf9ef2d9705541b4285cec1a589664028589b3
jinoc <choiae369@gmail.com>
2024-03-07 15:02:07 +0900

    Feat: 로그인 유지, 여부 확인 기능 추가
    - server 컴포넌트에서 supabase session값을 불러와 client 컴포넌트로
      전달하여 로그인 여부 확인
    - 로그인, 회원가입, 로그아웃 시 라우팅 필요

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+
 import Nav from '../nav/Nav';
 
-const Header = () => {
+import type { Database } from '@/lib/database.types';
+
+const Header = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <header className=" flex justify-center w-screen h-[88px]">
-      <Nav />
+      <Nav session={session} />
     </header>
   );
 };
